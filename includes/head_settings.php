81ea9a1dce4123fb9f20f4b9eedb68047d00aa8a
Inshu <137357069+BautistaCattaneo@users.noreply.github.com>
2023-09-11 13:06:32 -0300

    Update head_settings.php
    aceptalo pibe, poné bien las cositas. Te quiero amigo, sos capo

--- includes/head_settings.php
+++ includes/head_settings.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="http://localhost/sources/css/bootstrap.min.css">
     <link rel="stylesheet" href="http://localhost/sources/custom.css">
+    <script src="http://localhost/sources/js/bootstrap.bundle.js"></script>
     <script src="https://kit.fontawesome.com/e7401517de.js" crossorigin="anonymous"></script>
     <title><?php echo $title_name; ?></title>
 </head>
