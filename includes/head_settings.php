93ac14287d0522f89282a5a6651518ebf54dce50
Dalviux9th <enzod3339@gmail.com>
2023-09-16 15:40:37 -0300

    Adapto frontend al marco general de trabajo

--- includes/head_settings.php
+++ includes/head_settings.php
@@ -1,10 +1,59 @@
 <!DOCTYPE html>
-<html lang="en" data-bs-theme="<?php if ($dark_mode) echo 'dark'; else echo 'light'; ?>">
+<html lang="es" data-bs-theme="<?php if ($dark_mode) echo 'dark'; else echo 'light'; ?>">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="http://localhost/sources/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Cantarell" />
+    
+    <!--link rel="stylesheet" href="http://localhost/sources/css/bootstrap.min.css"-->
     <link rel="stylesheet" href="http://localhost/sources/custom.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
+    
     <script src="https://kit.fontawesome.com/e7401517de.js" crossorigin="anonymous"></script>
     <title><?php echo $title_name; ?></title>
+
 </head>
+
+<body>
+
+    <!--Navbar-->
+
+    <header class="header">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary p-4 navbar-left" id="menu">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><span class="text-muted">EEST N°2</span></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                        </li>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Biblioteca</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Historia</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+                            Más
+                        </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
--- index.php
+++ index.php
@@ -1,57 +1,9 @@
-<!doctype html>
-<html lang="en">
+<?php
+$title_name = 'Principal - EEST N°2 "Ing. Felipe Senillosa"'; // -- TITULO DE LA PAGINA EN EL NAVEGADOR
+include "includes/head_settings.php";
+?>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Cantarell" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <link rel="stylesheet" href="sources/custom.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
-    <title>EEST N°2 Ing. "Felipe Senillosa"</title>
-</head>
-
-<body>
-
-    <!--Navbar-->
-
-    <header class="header">
-        <nav class="navbar navbar-expand-lg bg-body-tertiary p-4 navbar-left" id="menu">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#"><span class="text-muted">EEST N°2</span></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Inicio</a>
-                        </li>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Biblioteca</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Historia</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
-                            Más
-                        </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+    <main>
 
         <!--Carousel de imagenes-->
 
@@ -76,10 +28,8 @@
                     <span class="visually-hidden">Next</span>
                 </button>
         </div>
-    </header>
 
 
-    <main>
 
         <!--Intro-->
 
@@ -169,26 +119,8 @@
             </div>
         </section>
 
-        <!--footer-->
-
-        <footer class="w-100 d-flex align-items justify-content-center">
-            <p class="fs-3 px-3 pt-3">
-                EEST N°2 Ing. "Felipe Senillosa"
-            </p>
-            <div class="" id="iconos">
-                <a href=""><i class="bi bi-instagram"></i></a>
-                <a href=""><i class="bi bi-twitter"></i></a>
-                <a href=""><i class="bi bi-facebook"></i></a>
-            </div>
-        </footer>
-
-
     </main>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-</body>
-
-</html>
+<?php
+include "includes/footer.php";
+?>
