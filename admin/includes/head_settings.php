efc02e437796f6a3b42d6f89a3d6dd8ea9fdaff5
BautistaCattaneo <bautistacattaneomiceli@gmail.com>
2023-10-03 19:24:13 -0300

    head_settings.php actualizado con la dirección de bootstrap

--- admin/includes/head_settings.php
+++ admin/includes/head_settings.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="http://localhost/sources/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://localhost/www/sources/css/bootstrap.min.css">
     <link rel="stylesheet" href="http://localhost/sources/custom.css">
     <script src="https://kit.fontawesome.com/e7401517de.js" crossorigin="anonymous"></script>
     <title><?php echo $title_name; ?></title>
