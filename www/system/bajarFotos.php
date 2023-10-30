e22a0fcb9483798fdb5f9d4605c8e4f25cb7d25a
Enzo Denicolay <116117642+Dalviux9th@users.noreply.github.com>
2023-10-30 01:49:45 -0300

    funcionalidades basicas de la busqueda (#4)
    Co-authored-by: MarcosTami <marquitostamim@gmail.com>

--- www/system/bajarFotos.php
+++ www/system/bajarFotos.php
@@ -4,9 +4,10 @@ include "imagen.php";
 
 $pass = $_POST["pass"];
 $id = $_POST["id"];
-$id2 = $id + 6;
+$sql = $_POST["sql"]
+
    if (isset($pass) && $pass == "1234") {
-    $res = mysqli_query($link,"SELECT `id_img`, `ubicacion` FROM `imagen` WHERE `id_img` > $id  AND   `id_img`< $id2" ); 
+    $res = mysqli_query($link,"SELECT `id_img`, `ubicacion` FROM `imagen` WHERE $id > `id_img` AND $sql LIMIT 20" ); 
     $jsonImg = array();
     while ($datos = mysqli_fetch_array($res)) {
         $img = new imagen($datos[0],$datos[1]);
