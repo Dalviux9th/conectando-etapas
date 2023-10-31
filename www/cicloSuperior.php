5b52d19c432e24283f69c8d5c9d92f8ceda6cd6f
BautistaCattaneo <bautistacattaneomiceli@gmail.com>
2023-10-31 06:51:52 -0300

    Cambios en el nav para linkear diferentes páginas. Especializaciones y ciclo superior mejorados y cambiados

--- www/cicloSuperior.php
+++ www/cicloSuperior.php
@@ -1,65 +1,219 @@
-<?php
-$title_name = "Ciclo Superior - EESTN°2";
-include "includes/head_settings.php";
-?>
-<main>
-    <div class="container">
+<!DOCTYPE html>
+<html lang="es" data-bs-theme="<?php if ($dark_mode) echo 'dark'; else echo 'light'; ?>">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Cantarell" />
+    
+    <link rel="stylesheet" href="http://localhost/www/sources/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://localhost/www/sources/main_config.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <link rel="stylesheet" href="http://localhost/www/sources/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
+    
+    <script src="https://kit.fontawesome.com/e7401517de.js" crossorigin="anonymous"></script>
+    <title>Ciclo Básico - EEST N°2 "Ing. Felipe Senillosa"</title>
+
+</head>
+
+<body>
+
+<header class="header">
+        <nav class="navbar navbar-expand-lg p-4 fixed-top" id="menu">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="./index.php"><span>EEST Nº2</span><img src="sources/images/logo_trans.png" alt="" class="img-header"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#extendNav" aria-controls="extendNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="extendNav">
+                    <ul class="navbar-nav mb-2 mb-lg-0 p-md-1">
+                        <li class="nav-item"><a class="nav-link" aria-current="page" href="./index.php">Inicio</a></li>
+                        <li class="nav-item" ><a class="nav-link" href="./anecdotasFront.php">Anécdotas</a></li>
+                        <li class="nav-item" ><a class="nav-link" href="./Front.php">Galería</a></li>
+                        <li class="nav-item" ><a class="nav-link" href="./historyFront.php">Historia</a></li>
+                        <li class="nav-item" ><a class="nav-link" href="./basicFront.php">Ciclo Básico</a></li>
+                        <li class="nav-item" ><a class="nav-link active" href="./cicloSuperior.php">Ciclo Superior</a></li>
+                        <li class="nav-item dropdown-center">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+                                Más información
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="./docsFront.php">Documentos</a></li>
+                                <li><a class="dropdown-item" href="./FAQ.php">FAQ</a></li>
+                                <li><a class="dropdown-item" href="./conEtFront.php">Conectando Etapas</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+
+<main class="container-fluid mt-5">
         <div class="row">
-            <div class="col-12 fs-2 fw-bold d-flex justify-content-center mb-3"><p>Ciclo Superior</p></div>
+            <h1 class="fs-2 fw-bold d-flex justify-content-center mb-3">Ciclo Superior</h1>
         </div>
+
         <div class="row d-flex">
-            <div class="col-12 col-sm-6 col-md-4 pb-4 d-flex justify-content-center">
-                <div class="card p-0" style="width: 24rem;">
-                    <a href="#" class="link-offset-2 link-underline link-underline-opacity-0">
-                    <img src="imagenes/automotores.jpg" class="card-img-top p-0 m-0" alt="...">
+            <div class="col-12 col-md-3 d-flex justify-content-center">
+                 <div class="card">
+                    <img src="imagenes/automotores.jpg" class="card-img-top p-0 m-0" alt="">
                     <div class="card-body">
-                        <h5 class="card-title ">Automotores</h5>
+                        <h4 class="card-title text-center">Cuarto Año</h4>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
-                </a>
                 </div>
-                
             </div>
-            <div class="col-12 col-sm-6 col-md-4 pb-4 d-flex justify-content-center">
-                <div class="card p-0" style="width: 24rem;">
-                    <a href="#" class="link-offset-2 link-underline link-underline-opacity-0">
+
+            <div class="col-12 col-md-3 d-flex justify-content-center">
+                 <div class="card">
                     <img src="imagenes/automotores.jpg" class="card-img-top p-0 m-0" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title ">Automotores</h5>
+                        <h5 class="card-title ">Quinto Año</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
-                </a>
                 </div>
-                
             </div>
-            <div class="col-12 col-sm-6 col-md-4 pb-4 d-flex justify-content-center">
-                <div class="card p-0" style="width: 24rem;">
-                    <a href="#" class="link-offset-2 link-underline link-underline-opacity-0">
+
+            <div class="col-12 col-md-3 d-flex justify-content-center">
+                 <div class="card">
                     <img src="imagenes/automotores.jpg" class="card-img-top p-0 m-0" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title ">Automotores</h5>
+                        <h5 class="card-title ">Sexto Año</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
-                </a>
                 </div>
-                
             </div>
-            <div class="col-12 col-sm-6 col-md-4 pb-4 d-flex justify-content-center">
-                <div class="card p-0" style="width: 24rem;">
-                    <a href="#" class="link-offset-2 link-underline link-underline-opacity-0">
+
+            <div class="col-12 col-md-3 d-flex justify-content-center">
+                <div class="card">
                     <img src="imagenes/automotores.jpg" class="card-img-top p-0 m-0" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title ">Automotores</h5>
+                        <h4 class="card-title">Séptimo Año</h4>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
-                </a>
                 </div>
-                
             </div>
-        
+        </div>
+
+    <hr>
+
+    <h1 class="text-center">Especializaciones</h1>
+    <p>Aquí podrá ver las diferentes elecciones de especializaciones, las cuales te otorgan la tecnicatura específica. Si quiere ver más información, haga click en alguna de las diferentes tarjetas</p>
+
+    <div class="row d-flex">
+            <div class="col-12 col-md-3 d-flex justify-content-center">
+                 <div class="card">
+                    <img src="imagenes/automotores.jpg" class="card-img-top p-0 m-0" alt="">
+                    <div class="card-body">
+                        <h4 class="card-title text-center">Programación</h4>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-3 d-flex justify-content-center">
+                 <div class="card">
+                    <img src="imagenes/automotores.jpg" class="card-img-top p-0 m-0" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title ">Quimica</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-3 d-flex justify-content-center">
+                 <div class="card">
+                    <img src="imagenes/automotores.jpg" class="card-img-top p-0 m-0" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title ">Maestro Mayor de Obra</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-3 d-flex justify-content-center">
+                <div class="card">
+                    <img src="imagenes/automotores.jpg" class="card-img-top p-0 m-0" alt="...">
+                    <div class="card-body">
+                        <h4 class="card-title">Electromecánica</h4>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-3 d-flex justify-content-center">
+                <div class="card">
+                    <img src="imagenes/automotores.jpg" class="card-img-top p-0 m-0" alt="...">
+                    <div class="card-body">
+                        <h4 class="card-title"></h4>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-3 d-flex justify-content-center">
+                <div class="card">
+                    <img src="imagenes/automotores.jpg" class="card-img-top p-0 m-0" alt="...">
+                    <div class="card-body">
+                        <h4 class="card-title">Automotores</h4>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-3 d-flex justify-content-center">
+                <div class="card">
+                    <img src="imagenes/automotores.jpg" class="card-img-top p-0 m-0" alt="...">
+                    <div class="card-body">
+                        <h4 class="card-title">Informática</h4>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+</main>
+
+
+
+    <div class="container-fluid" id="footer">
+        <div class="row text-center fs-5" id="footer">
+            <div class="col-12 mb-0 pt-0">
+                EEST N°2 "Ing. Felipe Senillosa"
+            </div>
+            <div class="col-12">
+                <small>
+                    Email: eestn2tandil@abc.gob.ar
+                </small>
+            </div>
+            <div class="col-12">
+                <small>
+                    Teléfono: 0249 - 4442637/33 
+                    Leando Alem 285
+                </small>
+            </div>
+            <div class="col-12 ">
+                <small id="iconos">
+                <a href="https://www.instagram.com/la.senillosa.eestn2.tandil/"><i class="bi bi-instagram"></i></a>
+                <a href="https://www.facebook.com/felipe.senillosa.9?locale=es_LA"><i class="bi bi-facebook"></i></a>
+                </small>
+            </div>
         </div>
     </div>
-</main>
-<?php
-include "includes/footer.php";
-?>
+
+
+    <script src="https://kit.fontawesome.com/5b34b9778f.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <script src="http://localhost/www/sources/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="http://localhost/www/sources/js/bootstrap.bundle.js"></script>
+    
+</body>
+</html>
